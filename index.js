6a6afb14eeae91b056018faee2db8306a1d09af8
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-10-02 10:50:04 +0300

    First Trial

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function capitalize(b) {
+  let c,d;
+  c = [...b];
+  d = (c.shift()).toUpperCase();
+  c.unshift(d);
+  return c.join("");
 }
+
+const titleCased = function () {
+  let newArr = new Array();
+  tutorials.map((tutorial1) => {
+      tutorial1 = tutorial1.split(" ");
+      let a = 0;
+      tutorial1.map((tutorial2) => {
+          tutorial2 = capitalize(tutorial2);
+          tutorial1.splice(a,1,tutorial2);
+          a++
+      })
+      tutorial1 = tutorial1.join(" ");
+      newArr.push(tutorial1);
+  })
+  return newArr;
+}
+
+
+console.log(titleCased());
